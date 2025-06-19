75a1d1718ce189af15cf4acbe7af22d1f70abfc6
Hyun <itsmine096@gmail.com>
2025-06-19 18:58:48 +0900

    Refresh Token 예제 프로젝트 생성

---
+++ refresh-token/src/main/java/com/hello/refresh_token/RefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.hello.refresh_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshTokenApplication.class, args);
+	}
+
+}
---
+++ refresh-token/src/test/java/com/hello/refresh_token/RefreshTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.refresh_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
